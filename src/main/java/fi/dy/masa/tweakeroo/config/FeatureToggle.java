0be24df3a516812e2941354a14383751fbd0af67
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2018-03-18 14:44:29 +0200

    Remove the unused bitmask from the FeatureToggle

--- src/main/java/fi/dy/masa/tweakeroo/config/FeatureToggle.java
+++ src/main/java/fi/dy/masa/tweakeroo/config/FeatureToggle.java
@@ -8,24 +8,22 @@ import fi.dy.masa.tweakeroo.config.interfaces.IKeybind;
 
 public enum FeatureToggle implements IConfigBoolean, IConfigHotkey
 {
-    TWEAK_SHULKERBOX_STACKING       ("tweakEmptyShulkerBoxesStack",         false, 0x0001, "X,H", "Enables empty Shulker Boxes stacking up to 64"),
-    TWEAK_FAST_BLOCK_PLACEMENT      ("tweakFastBlockPlacement",             false, 0x0002, "X,F", "Enables fast/convenient block placement when moving the cursor over new blocks"),
-    TWEAK_FLEXIBLE_BLOCK_PLACEMENT  ("tweakFlexibleBlockPlacement",         false, 0x0004, "X,L", "Enables placing blocks in different orientations while holding down the keybind"),
-    TWEAK_NO_FALLING_BLOCK_RENDER   ("tweakNoFallingBlockEntityRendering",  false, 0x0008, "X,R", "If enabled, then falling block entities won't be rendered at all"),
-    TWEAK_NO_ITEM_SWITCH_COOLDOWN   ("tweakNoItemSwitchRenderCooldown",     false, 0x0010, "X,I", "If true, then there won't be any cooldown/equip\nanimation when switching the held item or using the item.");
+    TWEAK_SHULKERBOX_STACKING       ("tweakEmptyShulkerBoxesStack",         false, "X,H", "Enables empty Shulker Boxes stacking up to 64"),
+    TWEAK_FAST_BLOCK_PLACEMENT      ("tweakFastBlockPlacement",             false, "X,F", "Enables fast/convenient block placement when moving the cursor over new blocks"),
+    TWEAK_FLEXIBLE_BLOCK_PLACEMENT  ("tweakFlexibleBlockPlacement",         false, "X,L", "Enables placing blocks in different orientations while holding down the keybind"),
+    TWEAK_NO_FALLING_BLOCK_RENDER   ("tweakNoFallingBlockEntityRendering",  false, "X,R", "If enabled, then falling block entities won't be rendered at all"),
+    TWEAK_NO_ITEM_SWITCH_COOLDOWN   ("tweakNoItemSwitchRenderCooldown",     false, "X,I", "If true, then there won't be any cooldown/equip\nanimation when switching the held item or using the item.");
 
     private final String name;
     private final String comment;
     private final String toggleMessage;
-    private final int bitMask;
     private boolean valueBoolean;
     private IKeybind keybind;
 
-    private FeatureToggle(String name, boolean defaultValue, int bitMask, String defaultHotkey, String comment)
+    private FeatureToggle(String name, boolean defaultValue, String defaultHotkey, String comment)
     {
         this.name = name;
         this.valueBoolean = defaultValue;
-        this.bitMask = bitMask;
         this.comment = comment;
         this.toggleMessage = "Toggled " + splitCamelCase(this.name.substring(5)) + " %s";
         this.keybind = KeybindMulti.fromStorageString(defaultHotkey);
@@ -61,12 +59,6 @@ public enum FeatureToggle implements IConfigBoolean, IConfigHotkey
     }
 
     @Override
-    public int getBitMask()
-    {
-        return this.bitMask;
-    }
-
-    @Override
     public IKeybind getKeybind()
     {
         return this.keybind;
@@ -76,34 +68,6 @@ public enum FeatureToggle implements IConfigBoolean, IConfigHotkey
     public void setKeybind(IKeybind keybind)
     {
         this.keybind = keybind;
-    }
-
-    /*
-    @Override
-    public String getHotkey()
-    {
-        return this.hotkey;
-    }
-
-    @Override
-    public void setHotkey(String hotkey)
-    {
-        this.hotkey = hotkey;
-    }
-    */
-
-    public int applyBitMask(int mask)
-    {
-        if (this.valueBoolean)
-        {
-            mask |= this.bitMask;
-        }
-        else
-        {
-            mask &= ~this.bitMask;
-        }
-
-        return mask;
     }
 
     @Override
--- src/main/java/fi/dy/masa/tweakeroo/config/interfaces/IConfigHotkey.java
+++ src/main/java/fi/dy/masa/tweakeroo/config/interfaces/IConfigHotkey.java
@@ -2,8 +2,6 @@ package fi.dy.masa.tweakeroo.config.interfaces;
 
 public interface IConfigHotkey extends IConfig
 {
-    int getBitMask();
-
     IKeybind getKeybind();
 
     void setKeybind(IKeybind keybind);
