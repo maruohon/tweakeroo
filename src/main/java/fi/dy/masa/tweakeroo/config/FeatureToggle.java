bf83d798748913a21ea8e1ef4e2b90dc42a7762c
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2018-09-11 02:38:26 +0300

    Change the config type of the FeatureToggle enum to fix the All Keybinds menu

--- src/main/java/fi/dy/masa/tweakeroo/config/FeatureToggle.java
+++ src/main/java/fi/dy/masa/tweakeroo/config/FeatureToggle.java
@@ -89,7 +89,7 @@ public enum FeatureToggle implements IHotkeyTogglable, IConfigValueNotifyable
     @Override
     public ConfigType getType()
     {
-        return ConfigType.BOOLEAN;
+        return ConfigType.HOTKEY;
     }
 
     @Override
