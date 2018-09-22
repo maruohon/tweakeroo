95390db1950e999d21e6604b02a101b1a707c2aa
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2018-09-22 18:04:23 +0300

    Move the shulker box util method to malilib

--- src/main/java/fi/dy/masa/tweakeroo/mixin/MixinItemShulkerBox.java
+++ src/main/java/fi/dy/masa/tweakeroo/mixin/MixinItemShulkerBox.java
@@ -1,8 +1,8 @@
 package fi.dy.masa.tweakeroo.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
+import fi.dy.masa.malilib.util.InventoryUtils;
 import fi.dy.masa.tweakeroo.config.FeatureToggle;
-import fi.dy.masa.tweakeroo.tweaks.StackingTweaks;
 import fi.dy.masa.tweakeroo.util.IItemStackLimit;
 import net.minecraft.block.Block;
 import net.minecraft.item.ItemBlock;
@@ -20,7 +20,7 @@ public class MixinItemShulkerBox extends ItemBlock implements IItemStackLimit
     @Override
     public int getItemStackLimit(ItemStack stack)
     {
-        if (FeatureToggle.TWEAK_SHULKERBOX_STACKING.getBooleanValue() && StackingTweaks.shulkerBoxHasItems(stack) == false)
+        if (FeatureToggle.TWEAK_SHULKERBOX_STACKING.getBooleanValue() && InventoryUtils.shulkerBoxHasItems(stack) == false)
         {
             return 64;
         }
