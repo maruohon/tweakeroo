ba1dcac11278eddb2544c4c404a6472e403ab404
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2018-10-03 00:51:46 +0300

    Add the no-slime-block-slowdown tweak from usefulmod

---
+++ src/main/java/fi/dy/masa/tweakeroo/mixin/MixinBlockSlime.java
@@ -0,0 +1,32 @@
+package fi.dy.masa.tweakeroo.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import fi.dy.masa.tweakeroo.config.FeatureToggle;
+import net.minecraft.block.BlockBreakable;
+import net.minecraft.block.BlockSlime;
+import net.minecraft.block.material.Material;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+@Mixin(BlockSlime.class)
+public abstract class MixinBlockSlime extends BlockBreakable
+{
+    protected MixinBlockSlime(Material materialIn, boolean ignoreSimilarityIn)
+    {
+        super(materialIn, ignoreSimilarityIn);
+    }
+
+    @Inject(method = "onEntityWalk", at = @At("HEAD"), cancellable = true)
+    private void onEntityWalkOnSlime(World worldIn, BlockPos pos, Entity entityIn, CallbackInfo ci)
+    {
+        if (FeatureToggle.TWEAK_NO_SLIME_BLOCK_SLOWDOWN.getBooleanValue())
+        {
+            super.onEntityWalk(worldIn, pos, entityIn);
+            ci.cancel();
+        }
+    }
+}
