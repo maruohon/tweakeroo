1edb98f75b635f1c711360586a9946192d9bb923
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2018-11-10 20:22:04 +0200

    Add a better/more comprehensive "no light updates on client" tweak

---
+++ src/main/java/fi/dy/masa/tweakeroo/mixin/MixinWorldClient.java
@@ -0,0 +1,41 @@
+package fi.dy.masa.tweakeroo.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import fi.dy.masa.tweakeroo.config.FeatureToggle;
+import net.minecraft.client.multiplayer.WorldClient;
+import net.minecraft.profiler.Profiler;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.EnumSkyBlock;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldProvider;
+import net.minecraft.world.storage.ISaveHandler;
+import net.minecraft.world.storage.WorldInfo;
+
+@Mixin(WorldClient.class)
+public abstract class MixinWorldClient extends World
+{
+    protected MixinWorldClient(ISaveHandler saveHandlerIn, WorldInfo info, WorldProvider providerIn, Profiler profilerIn, boolean client)
+    {
+        super(saveHandlerIn, info, providerIn, profilerIn, client);
+    }
+
+    @Override
+    public boolean checkLight(BlockPos pos)
+    {
+        if (FeatureToggle.TWEAK_NO_LIGHT_UPDATES_ALL.getBooleanValue() == false)
+        {
+            return super.checkLight(pos);
+        }
+
+        return false;
+    }
+
+    @Override
+    public void setLightFor(EnumSkyBlock type, BlockPos pos, int lightValue)
+    {
+        if (FeatureToggle.TWEAK_NO_LIGHT_UPDATES_ALL.getBooleanValue() == false)
+        {
+            super.setLightFor(type, pos, lightValue);
+        }
+    }
+}
