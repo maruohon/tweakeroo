46afc524cfa43fdc041667d43eff07c15a1619e4
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2019-01-20 02:03:08 +0200

    Add a tweak to remove mob spawner mob and particle rendering

---
+++ src/main/java/fi/dy/masa/tweakeroo/mixin/MixinTileEntityMobSpawnerRenderer.java
@@ -0,0 +1,21 @@
+package fi.dy.masa.tweakeroo.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import fi.dy.masa.tweakeroo.config.FeatureToggle;
+import net.minecraft.client.renderer.tileentity.TileEntityMobSpawnerRenderer;
+
+@Mixin(TileEntityMobSpawnerRenderer.class)
+public abstract class MixinTileEntityMobSpawnerRenderer
+{
+    @Inject(method = "render", at = @At("HEAD"), cancellable = true)
+    private void cancelRender(CallbackInfo ci)
+    {
+        if (FeatureToggle.TWEAK_NO_MOB_SPAWNER_MOB_RENDER.getBooleanValue())
+        {
+            ci.cancel();
+        }
+    }
+}
