1bf0fbad9f82d07c78eaf557aa7803ad690b5101
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2019-02-13 13:11:50 +0200

    Add a tweak to prevent TileEntity ticking

--- src/main/java/fi/dy/masa/tweakeroo/mixin/MixinWorld.java
+++ src/main/java/fi/dy/masa/tweakeroo/mixin/MixinWorld.java
@@ -3,10 +3,13 @@ package fi.dy.masa.tweakeroo.mixin;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import fi.dy.masa.tweakeroo.config.FeatureToggle;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 
 @Mixin(World.class)
@@ -19,5 +22,19 @@ public abstract class MixinWorld
         {
             ci.cancel();
         }
+    }
+
+    @Redirect(method = "updateEntities",
+            slice = @Slice(
+                    from = @At(value = "FIELD", target = "Lnet/minecraft/world/World;processingLoadedTiles:Z", ordinal = 0)),
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/tileentity/TileEntity;isInvalid()Z", ordinal = 0))
+    private boolean preventTileEntityTicking(TileEntity te)
+    {
+        if (FeatureToggle.TWEAK_NO_TILE_ENTITY_TICKING.getBooleanValue())
+        {
+            return true;
+        }
+
+        return te.isInvalid();
     }
 }
