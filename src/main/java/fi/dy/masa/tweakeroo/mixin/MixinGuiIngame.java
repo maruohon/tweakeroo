8efa149bef022f88603770d68b08b009b13e5b28
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2019-05-07 21:31:19 +0300

    Add a tweak to always render the player list

--- src/main/java/fi/dy/masa/tweakeroo/mixin/MixinGuiIngame.java
+++ src/main/java/fi/dy/masa/tweakeroo/mixin/MixinGuiIngame.java
@@ -1,18 +1,32 @@
 package fi.dy.masa.tweakeroo.mixin;
 
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import fi.dy.masa.tweakeroo.config.FeatureToggle;
 import fi.dy.masa.tweakeroo.renderer.RenderUtils;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiIngame;
+import net.minecraft.client.gui.GuiPlayerTabOverlay;
 import net.minecraft.client.gui.ScaledResolution;
+import net.minecraft.scoreboard.ScoreObjective;
+import net.minecraft.scoreboard.Scoreboard;
 
 @Mixin(GuiIngame.class)
 public abstract class MixinGuiIngame extends Gui
 {
+    @Shadow
+    @Final
+    private GuiPlayerTabOverlay overlayPlayerList;
+
+    @Shadow
+    @Final
+    private Minecraft mc;
+
     @Inject(method = "renderAttackIndicator", at = @At(value = "FIELD",
             target = "Lnet/minecraft/client/settings/GameSettings;showDebugInfo:Z", ordinal = 0), cancellable = true)
     private void overrideCursorRender(float partialTicks, ScaledResolution sr, CallbackInfo ci)
@@ -21,6 +35,24 @@ public abstract class MixinGuiIngame extends Gui
         {
             RenderUtils.renderDirectionsCursor(sr, this.zLevel, partialTicks);
             ci.cancel();
+        }
+    }
+
+    @Inject(method = "renderGameOverlay",
+            at = @At(value = "INVOKE",
+                     target = "Lnet/minecraft/client/gui/GuiPlayerTabOverlay;updatePlayerList(Z)V",
+                     ordinal = 0, shift = At.Shift.AFTER))
+    private void alwaysRenderPlayerList(float partialTicks, CallbackInfo ci)
+    {
+        if (FeatureToggle.TWEAK_PLAYER_LIST_ALWAYS_ON.getBooleanValue())
+        {
+            Scoreboard scoreboard = this.mc.world.getScoreboard();
+            ScoreObjective objective = scoreboard.getObjectiveInDisplaySlot(0);
+            ScaledResolution sr = new ScaledResolution(this.mc);
+            int width = sr.getScaledWidth();
+
+            this.overlayPlayerList.updatePlayerList(true);
+            this.overlayPlayerList.renderPlayerlist(width, scoreboard, objective);
         }
     }
 }
