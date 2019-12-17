5df467f4590707646b05550d7313cba974a0633c
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2019-12-17 17:13:11 +0200

    Fix the "player list always visible" Mixin in the built mod

--- src/main/java/fi/dy/masa/tweakeroo/mixin/MixinGuiIngame.java
+++ src/main/java/fi/dy/masa/tweakeroo/mixin/MixinGuiIngame.java
@@ -6,9 +6,6 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import fi.dy.masa.malilib.util.GuiUtils;
-import fi.dy.masa.tweakeroo.config.FeatureToggle;
-import fi.dy.masa.tweakeroo.renderer.RenderUtils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiIngame;
@@ -16,6 +13,9 @@ import net.minecraft.client.gui.GuiPlayerTabOverlay;
 import net.minecraft.client.gui.ScaledResolution;
 import net.minecraft.scoreboard.ScoreObjective;
 import net.minecraft.scoreboard.Scoreboard;
+import fi.dy.masa.malilib.util.GuiUtils;
+import fi.dy.masa.tweakeroo.config.FeatureToggle;
+import fi.dy.masa.tweakeroo.renderer.RenderUtils;
 
 @Mixin(GuiIngame.class)
 public abstract class MixinGuiIngame extends Gui
@@ -42,7 +42,7 @@ public abstract class MixinGuiIngame extends Gui
     @Inject(method = "renderGameOverlay",
             at = @At(value = "INVOKE",
                      target = "Lnet/minecraft/client/gui/GuiPlayerTabOverlay;updatePlayerList(Z)V",
-                     ordinal = 0, shift = At.Shift.AFTER))
+                     ordinal = 1, shift = At.Shift.AFTER)) // Note: The correct ordinal is 1 is the built mod, 0 in the 1.12.x dev environment...
     private void alwaysRenderPlayerList(float partialTicks, CallbackInfo ci)
     {
         if (FeatureToggle.TWEAK_PLAYER_LIST_ALWAYS_ON.getBooleanValue())
