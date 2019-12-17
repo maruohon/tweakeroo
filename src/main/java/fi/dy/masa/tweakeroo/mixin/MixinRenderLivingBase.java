315040e75d6b2228b2d86f2109cdc2047186c017
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2019-12-17 21:18:59 +0200

    Fix a crash with the Replaymod. Invisible entity rendering won't work with it.

--- src/main/java/fi/dy/masa/tweakeroo/mixin/MixinRenderLivingBase.java
+++ src/main/java/fi/dy/masa/tweakeroo/mixin/MixinRenderLivingBase.java
@@ -3,15 +3,15 @@ package fi.dy.masa.tweakeroo.mixin;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import fi.dy.masa.tweakeroo.config.FeatureToggle;
 import net.minecraft.client.renderer.entity.RenderLivingBase;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import fi.dy.masa.tweakeroo.config.FeatureToggle;
 
-@Mixin(RenderLivingBase.class)
+@Mixin(value = RenderLivingBase.class)
 public abstract class MixinRenderLivingBase
 {
-    @Redirect(method = "renderModel", at = @At(
+    @Redirect(method = "renderModel", require = 0, at = @At(
             value = "INVOKE",
             target = "Lnet/minecraft/entity/EntityLivingBase;isInvisibleToPlayer(Lnet/minecraft/entity/player/EntityPlayer;)Z"))
     private boolean redirectedIsInvisibleToPlayer(EntityLivingBase livingBase, EntityPlayer player)
