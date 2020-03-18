93346f0291b7da4912993545e55dcc6ea73cd77f
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2020-03-18 13:57:57 +0200

    Change the nether fog disable Mixin into an injection

--- src/main/java/fi/dy/masa/tweakeroo/mixin/MixinWorldProviderHell.java
+++ src/main/java/fi/dy/masa/tweakeroo/mixin/MixinWorldProviderHell.java
@@ -1,16 +1,20 @@
 package fi.dy.masa.tweakeroo.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import fi.dy.masa.tweakeroo.config.Configs;
-import net.minecraft.world.WorldProvider;
-import net.minecraft.world.WorldProviderHell;
 
-@Mixin(WorldProviderHell.class)
-public abstract class MixinWorldProviderHell extends WorldProvider
+@Mixin(net.minecraft.world.WorldProviderHell.class)
+public abstract class MixinWorldProviderHell extends net.minecraft.world.WorldProvider
 {
-    @Override
-    public boolean doesXZShowFog(int x, int z)
+    @Inject(method = "doesXZShowFog", at = @At("HEAD"), cancellable = true)
+    public void disableNetherFog(int x, int z, CallbackInfoReturnable<Boolean> cir)
     {
-        return ! Configs.Disable.DISABLE_NETHER_FOG.getBooleanValue();
+        if (Configs.Disable.DISABLE_NETHER_FOG.getBooleanValue())
+        {
+            cir.setReturnValue(false);
+        }
     }
 }
