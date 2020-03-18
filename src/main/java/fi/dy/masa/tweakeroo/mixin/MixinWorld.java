f6ebd87eca4c6e5aeced67f5369a30715c103a8e
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2020-03-18 22:35:49 +0200

    Use fully qualified class names in MixinWorld

--- src/main/java/fi/dy/masa/tweakeroo/mixin/MixinWorld.java
+++ src/main/java/fi/dy/masa/tweakeroo/mixin/MixinWorld.java
@@ -13,30 +13,21 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import fi.dy.masa.tweakeroo.config.Configs;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.tileentity.TileEntity;
-import net.minecraft.world.World;
 
-@Mixin(World.class)
+@Mixin(net.minecraft.world.World.class)
 public abstract class MixinWorld
 {
-    @Shadow
-    @Final
-    public List<TileEntity> loadedTileEntityList;
+    @Shadow @Final public List<net.minecraft.tileentity.TileEntity> loadedTileEntityList;
+    @Shadow @Final public List<net.minecraft.tileentity.TileEntity> tickableTileEntities;
+    @Shadow @Final private List<net.minecraft.tileentity.TileEntity> tileEntitiesToBeRemoved;
 
-    @Shadow
-    @Final
-    public List<TileEntity> tickableTileEntities;
 
-    @Shadow
-    @Final
-    private List<TileEntity> tileEntitiesToBeRemoved;
 
     @Inject(method = "updateEntityWithOptionalForce", at = @At("HEAD"), cancellable = true)
-    private void preventEntityTicking(Entity entityIn, boolean forceUpdate, CallbackInfo ci)
+    private void preventEntityTicking(net.minecraft.entity.Entity entityIn, boolean forceUpdate, CallbackInfo ci)
     {
-        if (Configs.Disable.DISABLE_ENTITY_TICKING.getBooleanValue() && (entityIn instanceof EntityPlayer) == false)
+        if (Configs.Disable.DISABLE_ENTITY_TICKING.getBooleanValue() &&
+            (entityIn instanceof net.minecraft.entity.player.EntityPlayer) == false)
         {
             ci.cancel();
         }
@@ -46,7 +37,7 @@ public abstract class MixinWorld
             slice = @Slice(
                     from = @At(value = "FIELD", target = "Lnet/minecraft/world/World;processingLoadedTiles:Z", ordinal = 0)),
             at = @At(value = "INVOKE", target = "Lnet/minecraft/tileentity/TileEntity;isInvalid()Z", ordinal = 0))
-    private boolean preventTileEntityTicking(TileEntity te)
+    private boolean preventTileEntityTicking(net.minecraft.tileentity.TileEntity te)
     {
         if (Configs.Disable.DISABLE_TILE_ENTITY_TICKING.getBooleanValue())
         {
@@ -57,15 +48,15 @@ public abstract class MixinWorld
     }
 
 
-    @Inject(method = "updateEntities",
-            at = @At(value = "FIELD", target = "Lnet/minecraft/world/World;tileEntitiesToBeRemoved:Ljava/util/List;", ordinal = 0))
+    @Inject(method = "updateEntities", at = @At(value = "FIELD", ordinal = 0,
+                target = "Lnet/minecraft/world/World;tileEntitiesToBeRemoved:Ljava/util/List;"))
     private void optimizedTileEntityRemoval(CallbackInfo ci)
     {
         if (Configs.Fixes.TILE_UNLOAD_OPTIMIZATION.getBooleanValue())
         {
             if (this.tileEntitiesToBeRemoved.isEmpty() == false)
             {
-                Set<TileEntity> remove = Collections.newSetFromMap(new IdentityHashMap<>());
+                Set<net.minecraft.tileentity.TileEntity> remove = Collections.newSetFromMap(new IdentityHashMap<>());
                 remove.addAll(this.tileEntitiesToBeRemoved);
                 this.tickableTileEntities.removeAll(remove);
                 this.loadedTileEntityList.removeAll(remove);
