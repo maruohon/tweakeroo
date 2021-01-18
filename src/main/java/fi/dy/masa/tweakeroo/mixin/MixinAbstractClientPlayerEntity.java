3329836a1ff398ab13cd7ebd433b7c3c2622adbc
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2021-01-18 16:44:40 +0200

    Free Camera: Fix clipping through the world with Optifine

---
+++ src/main/java/fi/dy/masa/tweakeroo/mixin/MixinAbstractClientPlayerEntity.java
@@ -0,0 +1,22 @@
+package fi.dy.masa.tweakeroo.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import net.minecraft.client.network.AbstractClientPlayerEntity;
+import fi.dy.masa.tweakeroo.config.FeatureToggle;
+import fi.dy.masa.tweakeroo.util.CameraUtils;
+
+@Mixin(AbstractClientPlayerEntity.class)
+public class MixinAbstractClientPlayerEntity
+{
+    @Inject(method = "isSpectator", at = @At("HEAD"), cancellable = true)
+    private void overrideIsSpectator(CallbackInfoReturnable<Boolean> cir)
+    {
+        if (FeatureToggle.TWEAK_FREE_CAMERA.getBooleanValue() && CameraUtils.getFreeCameraSpectator())
+        {
+            cir.setReturnValue(true);
+        }
+    }
+}
