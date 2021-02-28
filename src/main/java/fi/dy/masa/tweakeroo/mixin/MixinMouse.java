47e3bcec96a9b6dbdfae538eceec1f5e4b191c05
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2021-02-28 14:54:22 +0200

    Change the Mouse mixin from the previous commit to edit the vertical amount

--- src/main/java/fi/dy/masa/tweakeroo/mixin/MixinMouse.java
+++ src/main/java/fi/dy/masa/tweakeroo/mixin/MixinMouse.java
@@ -1,8 +1,6 @@
 package fi.dy.masa.tweakeroo.mixin;
 
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import net.minecraft.client.MinecraftClient;
@@ -12,18 +10,14 @@ import fi.dy.masa.tweakeroo.config.Configs;
 @Mixin(Mouse.class)
 public class MixinMouse
 {
-    @Shadow @Final private MinecraftClient client;
-
-    @ModifyVariable(method = "onMouseScroll", ordinal = 2, at = @At(value = "FIELD", ordinal = 0,
-                    target = "Lnet/minecraft/client/MinecraftClient;currentScreen:Lnet/minecraft/client/gui/screen/Screen;"))
-    private double applyHorizontalScroll(double delta, long window, double horizontal, double vertical)
+    @ModifyVariable(method = "onMouseScroll", ordinal = 1, at = @At("HEAD"))
+    private double applyHorizontalScroll(double vertical, long argWindow, double argHorizontal, double argVertical)
     {
         if (Configs.Fixes.MAC_HORIZONTAL_SCROLL.getBooleanValue() && MinecraftClient.IS_SYSTEM_MAC && vertical == 0)
         {
-            vertical = horizontal;
-            delta = (this.client.options.discreteMouseScroll ? Math.signum(vertical) : vertical) * this.client.options.mouseWheelSensitivity;
+            return argHorizontal;
         }
 
-        return delta;
+        return vertical;
     }
 }
