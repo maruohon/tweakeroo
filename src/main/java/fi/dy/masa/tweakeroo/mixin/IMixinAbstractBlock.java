fe6f4dd943303e1ab6a5c6ce61629fae964816d9
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2021-06-04 15:50:42 +0300

    Fix the noSlimeBlockSlowdown tweak crashing when activated

--- src/main/java/fi/dy/masa/tweakeroo/mixin/IMixinAbstractBlock.java
+++ src/main/java/fi/dy/masa/tweakeroo/mixin/IMixinAbstractBlock.java
@@ -1,11 +1,13 @@
 package fi.dy.masa.tweakeroo.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(net.minecraft.block.AbstractBlock.class)
 public interface IMixinAbstractBlock
 {
+    @Mutable
     @Accessor("slipperiness")
     void setFriction(float friction);
 }
