b2fdd4330eee8bbc78f6988ccb8951ea573c8c36
Hendrix-Shen <HendrixShen@qq.com>
2021-06-14 13:34:01 +0800

    Fix the crash caused by `tweakChatPersistentText`

--- src/main/java/fi/dy/masa/tweakeroo/mixin/MixinChatScreen.java
+++ src/main/java/fi/dy/masa/tweakeroo/mixin/MixinChatScreen.java
@@ -1,6 +1,8 @@
 package fi.dy.masa.tweakeroo.mixin;
 
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.At.Shift;
@@ -21,8 +23,9 @@ public abstract class MixinChatScreen
 {
     @Shadow
     protected TextFieldWidget chatField;
-    @Shadow
-    private String originalChatText;
+
+    @Mutable
+    @Shadow @Final private String originalChatText;
 
     @Inject(method = "removed", at = @At("HEAD"))
     private void storeChatText(CallbackInfo ci)
