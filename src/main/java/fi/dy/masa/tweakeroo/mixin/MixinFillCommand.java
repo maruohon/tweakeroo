59ceb174b92040c2aeaec5634c375a99006b0bb2
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2023-07-03 19:51:48 +0300

    Fix/update the fillCloneLimit tweak

--- src/main/java/fi/dy/masa/tweakeroo/mixin/MixinFillCommand.java
+++ src/main/java/fi/dy/masa/tweakeroo/mixin/MixinFillCommand.java
@@ -1,23 +1,30 @@
 package fi.dy.masa.tweakeroo.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
 import net.minecraft.server.command.FillCommand;
+import net.minecraft.world.GameRules;
+import net.minecraft.world.GameRules.IntRule;
+import net.minecraft.world.GameRules.Key;
+
 import fi.dy.masa.tweakeroo.config.Configs;
 import fi.dy.masa.tweakeroo.config.FeatureToggle;
 
-@Mixin(value = FillCommand.class, priority = 999)
+@Mixin(value = FillCommand.class, priority = 1001)
 public abstract class MixinFillCommand
 {
-    @ModifyConstant(method = "execute", constant = @Constant(intValue = 32768), require = 0)
-    private static int getBlockCountLimit(int original)
+    @Redirect(method = "execute", require = 0,
+              at = @At(value = "INVOKE",
+                       target = "Lnet/minecraft/world/GameRules;getInt(Lnet/minecraft/world/GameRules$Key;)I"))
+    private static int tweakeroo_overrideBlockLimit(GameRules instance, Key<IntRule> rule)
     {
         if (FeatureToggle.TWEAK_FILL_CLONE_LIMIT.getBooleanValue())
         {
             return Configs.Generic.FILL_CLONE_LIMIT.getIntegerValue();
         }
 
-        return original;
+        return instance.getInt(rule);
     }
 }
