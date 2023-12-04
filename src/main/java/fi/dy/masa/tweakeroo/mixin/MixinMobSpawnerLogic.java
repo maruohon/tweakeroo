6dee605d935ef569a3018d81442fc7075d9f2f18
Sakura Ryoko <116967773+sakura-ryoko@users.noreply.github.com>
2023-12-03 23:14:50 -0500

    1.20.3-rc.1 changes
    MixinMobSpawnerLogic

--- src/main/java/fi/dy/masa/tweakeroo/mixin/MixinMobSpawnerLogic.java
+++ src/main/java/fi/dy/masa/tweakeroo/mixin/MixinMobSpawnerLogic.java
@@ -5,7 +5,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.MobSpawnerLogic;
+import net.minecraft.block.spawner.MobSpawnerLogic;
 import net.minecraft.world.World;
 import fi.dy.masa.tweakeroo.config.Configs;
 
