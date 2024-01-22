a8807d5eed8023d8fd1ee8e30885968aa6d63e2b
Sakura Ryoko <116967773+sakura-ryoko@users.noreply.github.com>
2024-01-21 19:52:18 -0500

    Update WorldLoadListener.java

--- src/main/java/fi/dy/masa/tweakeroo/event/WorldLoadListener.java
+++ src/main/java/fi/dy/masa/tweakeroo/event/WorldLoadListener.java
@@ -1,7 +1,6 @@
 package fi.dy.masa.tweakeroo.event;
 
 import javax.annotation.Nullable;
-
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.world.ClientWorld;
 import fi.dy.masa.malilib.interfaces.IWorldLoadListener;
@@ -9,16 +8,15 @@ import fi.dy.masa.tweakeroo.config.FeatureToggle;
 
 public class WorldLoadListener implements IWorldLoadListener
 {
-    // Hacky toggle for "fixing" gamma state automatically if left on from previous play session
     private boolean gammaEnabled = false;
     @Override
     public void onWorldLoadPre(@Nullable ClientWorld worldBefore, @Nullable ClientWorld worldAfter, MinecraftClient mc)
     {
         // Always disable the Free Camera mode when leaving the world or switching dimensions
         FeatureToggle.TWEAK_FREE_CAMERA.setBooleanValue(false);
-        if (FeatureToggle.TWEAK_GAMMA_OVERRIDE.getBooleanValue()) {
+        if (FeatureToggle.TWEAK_GAMMA_OVERRIDE.getBooleanValue())
+        {
             gammaEnabled = false;
-            //Tweakeroo.debugLog("WorldLoadListener#onWorldLoadPre(): tweak of TWEAK_GAMMA_OVERRIDE = false.");
         }
     }
     @Override
@@ -28,7 +26,7 @@ public class WorldLoadListener implements IWorldLoadListener
         {
             if (!gammaEnabled)
             {
-                //Tweakeroo.debugLog("WorldLoadListener#onWorldLoadPost(): tweaking TWEAK_GAMMA_OVERRIDE from false -> true.");
+                // Hacky toggle for "fixing" gamma state automatically if left on from previous play session
                 FeatureToggle.TWEAK_GAMMA_OVERRIDE.setBooleanValue(false);
                 FeatureToggle.TWEAK_GAMMA_OVERRIDE.setBooleanValue(true);
                 gammaEnabled = true;
