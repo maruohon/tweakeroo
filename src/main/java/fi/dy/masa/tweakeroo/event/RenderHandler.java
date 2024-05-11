b06953a4239c5814f8af2e855ed56f4b25959a79
Sakura Ryoko <116967773+sakura-ryoko@users.noreply.github.com>
2024-05-11 14:28:15 -0400

    Update RenderHandler.java

--- src/main/java/fi/dy/masa/tweakeroo/event/RenderHandler.java
+++ src/main/java/fi/dy/masa/tweakeroo/event/RenderHandler.java
@@ -85,7 +85,7 @@ public class RenderHandler implements IRenderer
         else if (stack.getComponents().contains(DataComponentTypes.CONTAINER) && InventoryUtils.shulkerBoxHasItems(stack))
         {
             if (FeatureToggle.TWEAK_SHULKERBOX_DISPLAY.getBooleanValue() &&
-                (Configs.Generic.SHULKER_DISPLAY_REQUIRE_SHIFT.getBooleanValue() == false || GuiBase.isShiftDown())
+                (Configs.Generic.SHULKER_DISPLAY_REQUIRE_SHIFT.getBooleanValue() == false || GuiBase.isShiftDown()))
             {
                 fi.dy.masa.malilib.render.RenderUtils.renderShulkerBoxPreview(stack, x, y, Configs.Generic.SHULKER_DISPLAY_BACKGROUND_COLOR.getBooleanValue(), drawContext);
             }
