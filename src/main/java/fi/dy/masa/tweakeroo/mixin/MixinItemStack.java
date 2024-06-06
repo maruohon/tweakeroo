3eb471cd6c76b8aa355780a4bfdff60ca99e8101
Sakura Ryoko <116967773+sakura-ryoko@users.noreply.github.com>
2024-06-05 23:39:08 -0400

    FIX ItemStack.getMaxCount()

--- src/main/java/fi/dy/masa/tweakeroo/mixin/MixinItemStack.java
+++ src/main/java/fi/dy/masa/tweakeroo/mixin/MixinItemStack.java
@@ -5,6 +5,8 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import net.minecraft.component.ComponentMap;
+import net.minecraft.component.DataComponentTypes;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import fi.dy.masa.tweakeroo.util.IItemStackLimit;
@@ -15,9 +17,11 @@ public abstract class MixinItemStack
     @Shadow
     public abstract Item getItem();
 
-    @Inject(method = "getMaxCount", at = @At("HEAD"), cancellable = true)
-    public void getMaxStackSizeStackSensitive(CallbackInfoReturnable<Integer> ci)
+    @Shadow public abstract ComponentMap getComponents();
+
+    @Inject(method = "getMaxCount", at = @At("RETURN"), cancellable = true)
+    public void getMaxStackSizeStackSensitive(CallbackInfoReturnable<Integer> cir)
     {
-        ci.setReturnValue(((IItemStackLimit) this.getItem()).getMaxStackSize((ItemStack) (Object) this));
+        cir.setReturnValue(Math.max(this.getComponents().getOrDefault(DataComponentTypes.MAX_STACK_SIZE, 1), ((IItemStackLimit) this.getItem()).getMaxStackSize((ItemStack) (Object) this)));
     }
 }
