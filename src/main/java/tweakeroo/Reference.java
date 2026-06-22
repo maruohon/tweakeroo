1384fa0d29b2aa57072654bcd27b6562db872b74
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2026-06-22 12:23:46 +0300

    Fix using the old Forge/LiteLoader version number placeholder

--- src/main/java/tweakeroo/Reference.java
+++ src/main/java/tweakeroo/Reference.java
@@ -1,12 +1,13 @@
 package tweakeroo;
 
+import malilib.util.StringUtils;
 import malilib.util.data.ModInfo;
 
 public class Reference
 {
     public static final String MOD_ID = "tweakeroo";
     public static final String MOD_NAME = "Tweakeroo";
-    public static final String MOD_VERSION = "@MOD_VERSION@";
+    public static final String MOD_VERSION = StringUtils.getModVersionString(MOD_ID);
 
     public static final ModInfo MOD_INFO = new ModInfo(MOD_ID, MOD_NAME);
 }
